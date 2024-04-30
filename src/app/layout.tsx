0ac15b32b320f1d2efa4a2ebcb0204a8caa908a6
n0rrman <email@henriknorrman.com>
2024-05-01 01:06:26 +0700

    meta content added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "henriknorrman.com",
   description: "Hello 👋, I'm Henrik Norrman.",
+  metadataBase: new URL("https://henriknorrman.com"),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
+  twitter: {
+    images: "/twitter-image.png",
+  },
 };
 
 export const viewport: Viewport = {
