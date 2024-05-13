fb94381983bd05ef004bbfb5fab6cc7e7fbab586
n0rrman <email@henriknorrman.com>
2024-05-13 23:01:40 +0700

    new commands & code refactor

---
+++ src/actions/action-codes.ts
@@ -0,0 +1,8 @@
+export enum ActionCode {
+    NOOP = "noop",
+    CLEAR = "clear",
+    REDIRECT = "redirect",
+    ERROR = "error",
+    EXIT = "exit",
+    HELP = "help",
+}
--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,54 +1,54 @@
 "use client";
 
-import Image from "next/image";
-
-import { courier } from "../app/fonts";
-
-import screenImg from "/public/screen.svg";
 import { useFormState } from "react-dom";
 import { useState, startTransition, useRef, useEffect } from "react";
-import { terminalState } from "@/actions";
+import { redirect } from "next/navigation";
+
+import { terminalState, CommandAction } from "@/actions";
+import { ActionCode } from "@/actions/action-codes";
+import TerminalScreen from "./terminal-screen";
 
 export default function Terminal() {
-  const [outputs, setOutputs] = useState([{ id: "0", input: "", output: "" }]);
-  const [input, setInput] = useState("whoami");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const formRef = useRef<HTMLFormElement | null>(null);
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [outputs, setOutputs] = useState<CommandAction[]>([]);
   const [formState, action] = useFormState(terminalState, {});
 
-  const prompt = <span>henriknorrman.com/&gt;</span>;
-
+  // Initial whoami command
   useEffect(() => {
-    formRef.current!.requestSubmit();
+    inputRef.current!.value = "whoami";
     inputRef.current!.focus();
+    formRef.current!.requestSubmit();
   }, []);
 
+  // On new submit -> add to outputs & handle command
   useEffect(() => {
     if (formState.payload) {
-      const { id, input, output } = formState.payload;
-      setOutputs((outputs) => [...outputs, { id, input, output }]);
+      const { id, input, output, actionCode } = formState.payload;
+
+      setOutputs((outputs) => [...outputs!, { id, input, output, actionCode }]);
+      handleTerminalAction(output, actionCode);
     }
   }, [formState]);
 
-  const renderedOutputs = outputs.map(({ id, input, output }) => {
-    return (
-      <div
-        className={`flex flex-col gap-y-0.5 font-thin ${courier.className}`}
-        key={id}
-      >
-        <p>
-          {prompt}
-          {input}
-        </p>
-        {output !== "" && <p className="">{output}</p>}
-      </div>
-    );
-  });
+  // Handle action codes
+  const handleTerminalAction = (value: string, actionCode: string) => {
+    if (actionCode === ActionCode.CLEAR) {
+      setOutputs([]);
+    }
+    if (actionCode === ActionCode.REDIRECT) {
+      redirect(value);
+    }
+    if (actionCode === ActionCode.EXIT) {
+      setOutputs([]);
+      window.history.back();
+    }
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setInput("");
+  // Input submit handler
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
     if (formRef.current) {
       const formData = new FormData(formRef.current);
@@ -57,43 +57,17 @@ export default function Terminal() {
         action(formData);
       });
     }
+    inputRef!.current!.value = "";
   };
 
+  const prompt = <span>henriknorrman.com/&gt;</span>;
   return (
-    <div className="flex justify-center items-center text-[1.77vw] md:text-xs lg:text-sm w-full">
-      <div
-        onClick={() => {
-          inputRef.current?.focus();
-        }}
-        className="flex justify-center items-center absolute overflow-hidden"
-      >
-        <div className="relative w-[104vw] -mx-[2vw] md:mx-0 md:w-[93.5ch] aspect-[1.25] pointer-events-none select-none">
-          <Image alt="" src={screenImg} fill></Image>
-        </div>
-      </div>
-      <div
-        onClick={() => {
-          inputRef.current?.focus();
-        }}
-        className={`${courier.className} px-[1.5ch] py-[0.5ch] mb-[13ch] bg-slate-800 text-slate-200 flex items-end w-[90ch] leading-snug tracking-wide h-[57ch] overflow-hidden`}
-      >
-        <form ref={formRef} onSubmit={handleSubmit}>
-          <pre className="space-y-[0.25ch]">{renderedOutputs}</pre>
-          <div className="flex flex-row items-end justify-start">
-            {prompt}
-            <input
-              ref={inputRef}
-              className="bg-transparent w-[67ch] outline-none mt-[0.25ch]"
-              value={input}
-              onChange={(event) => {
-                setInput(event.target.value);
-              }}
-              name="cmd"
-              type="text"
-            />
-          </div>
-        </form>
-      </div>
-    </div>
+    <TerminalScreen
+      prompt={prompt}
+      outputs={outputs}
+      inputRef={inputRef}
+      formRef={formRef}
+      submitHandler={handleSubmit}
+    />
   );
 }
